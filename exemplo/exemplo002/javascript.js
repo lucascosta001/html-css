7a878b6c01f3298cc743b50758b6a38ef6901e2f
Lucas Alan Costa Novais <107582861+lucascosta001@users.noreply.github.com>
2025-03-12 14:23:17 -0300

    tentativa de fazer uma barra de pesquisa

--- exemplo/exemplo002/javascript.js
+++ exemplo/exemplo002/javascript.js
@@ -1,6 +1,11 @@
-let search = document.getElementById('search')
-let resul = document.getElementById('result')
+function search() {
+    let pesquisar = document.getElementById('search')
+    pesquisar = pesquisar.toLowerCase()
+    let itens = document.getElementsByClassName('names')
 
-function (params) {
-    
+    for (let i = 0; i < itens.length; i++) {
+        if (!itens[i].innerHTML.toLocaleLowerCase().includes(pesquisar)) {
+            itens[i].style.display = "none"
+        }
+    }
 }
