87c7307d0b0b5c17d69e62b4686e3c4b3c9d6e7c
Lucas Alan Costa Novais <107582861+lucascosta001@users.noreply.github.com>
2025-05-27 22:21:42 -0300

    updates

---
+++ javascript.js
@@ -0,0 +1,15 @@
+let arq = document.getElementsByClassName("secelementos");
+
+// pasta principal
+// pegar cada aquivo, porém não são todos que estão listados como index.html, logo vou precisar de um arquivo que busque pelo tipo de arquivo, que é o .html
+for (let i = 0; i < arq.length; i++) {
+    let arquivo = arq[i].getElementsByTagName("a")[0];
+    if (arquivo) {
+        let link = arquivo.href;
+        if (link.endsWith(".html")) {
+            console.log("Arquivo HTML encontrado: " + link);
+        } else {
+            console.log("Arquivo não é HTML: " + link);
+        }
+    }
+}
